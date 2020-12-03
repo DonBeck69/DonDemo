61fcee6ab97e008ea8eafc892d53f79550851d64
Donovan Beck <donovan.beck.69@outlook.com>
2020-12-03 13:40:27 -0800

    publiish and code clean

--- DemoDocFunction/DocFunction.cs
+++ DemoDocFunction/DocFunction.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Net.Http;
-using Microsoft.Net.Http.Headers;
-//using Microsoft.Extensions.Primitives;
+using System.Web.Http;
 using Newtonsoft.Json;
 using PdfBuilder;
 using DemoObjects;
@@ -23,31 +20,34 @@ namespace DemoDocFunction
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-            DocBuilder docBuilder = new DocBuilder();
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            PdfData pdfData = JsonConvert.DeserializeObject<PdfData>(requestBody);
-            byte[] bits = await Task.Run(() => docBuilder.Build(pdfData));
-
-            HttpResponseMessage httpResponse = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            try
             {
-                Content = new ByteArrayContent(bits)
-            };
+                //log.LogInformation("C# HTTP trigger function processed a request.");
+                DocBuilder docBuilder = new DocBuilder();
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                PdfData pdfData = JsonConvert.DeserializeObject<PdfData>(requestBody);
+                byte[] bits = await Task.Run(() => docBuilder.Build(pdfData));
 
-            
-            httpResponse.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/pdf");
-
-            return httpResponse;
-
-
-            //return new FileContentResult(bits, new MediaTypeHeaderValue("application/octet-stream")) 
-            //{
-            //    FileDownloadName = "Test.pdf"
-
-            //};
+                HttpResponseMessage httpResponse = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+                {
+                    Content = new ByteArrayContent(bits)
+                };
 
 
-            //application/pdf
+                httpResponse.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/pdf");
+
+                return httpResponse;
+            }
+            catch(Exception ex)
+            {
+                HttpResponseMessage resp = new HttpResponseMessage(System.Net.HttpStatusCode.InternalServerError)
+                {
+                    Content = new StringContent(ex.Message),
+                    ReasonPhrase = "PDF Function Error"
+                };
+                throw new HttpResponseException(resp);
+            }
+
         }
     }
 }
